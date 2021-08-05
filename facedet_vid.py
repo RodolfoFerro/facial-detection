d1dc0ea06fe6518b1bb3fc1835e3749d673dcdce
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2021-08-04 19:08:14 -0500

    Updated base code for RT video. :gear:

--- facedet_vid.py
+++ facedet_vid.py
@@ -2,28 +2,31 @@ import numpy as np
 import imutils
 import cv2
 
-# Load Haar cascade files:
+# Load Haar Cascade files
 path_cascade = "./haarcascades/"
 face_cascade = cv2.CascadeClassifier(
-    path_cascade + 'haarcascade_frontalface_default.xml')
+    path_cascade + 'haarcascade_frontalface_default.xml'
+)
 eye_cascade = cv2.CascadeClassifier(
-    path_cascade + 'haarcascade_mcs_eyepair_big.xml')
+    path_cascade + 'haarcascade_mcs_eyepair_big.xml'
+)
 
-# Load video capture:
+# Load video capture
 cap = cv2.VideoCapture(0)
 
-while(True):
-    # Capture frame-by-frame:
+while True:
+    # Capture frame-by-frame
     ret, img = cap.read()
     img = imutils.resize(img, width=500)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # Create multiscale classifier:
+    # Create multiscale classifier
     faces = face_cascade.detectMultiScale(gray, 1.8, 5)
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
         roi_gray = gray[y:y + h, x:x + w]
         roi_color = img[y:y + h, x:x + w]
+        
         eyes = eye_cascade.detectMultiScale(roi_gray)
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey),
